55faf42f226f8f8c2a73274116e5eee9dcea6edb
GivenCingco <givencingco@gmail.com>
2024-04-24 14:25:00 +0200

    Initial commit

--- solvyng-archivyng/frontend/src/App.jsx
+++ solvyng-archivyng/frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { Amplify } from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
 import ResetPassword from './components/auth/reset-password';
 import ConfirmResetPassword from './components/auth/confirm-reset-password';
+import Dashboard from './components/pages/Dashboard';
 //Amplify environment is to be changed before running auth pages
 
 Amplify.configure(amplifyconfig);
@@ -16,7 +17,8 @@ Amplify.configure(amplifyconfig);
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Dashboard />} />
+      {/* <Route path="/" element={<HomePage />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/verify" element={<Verify />} />
--- solvyng-archivyng/frontend/src/components/pages/homepage.jsx
+++ solvyng-archivyng/frontend/src/components/pages/homepage.jsx
@@ -1,26 +1,49 @@
 // import React, { useState } from 'react';
-import './styling/homepage.css';
-import { signOut } from 'aws-amplify/auth';
-import { useNavigate } from 'react-router-dom';
+import { Dashboard } from './Dashboard';
+// import { signOut } from 'aws-amplify/auth';
+// import { useNavigate } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
-  async function handleSignOut() {
-    try {
-      await signOut()
-      console.log("Logout works");
-      navigate("/login");
-    } catch (error) {
-      console.log('error signing out: ', error);
-    }
-  }
-  return (
-    <><h1>Landing Page</h1><div>
-        <button type="submit" className="button-landpage" onClick={handleSignOut}> Log Out </button>
-      </div></>
+  // async function handleSignOut() {
+  //   try {
+  //     await signOut()
+  //     console.log("Logout works");
+  //     navigate("/login");
+  //   } catch (error) {
+  //     console.log('error signing out: ', error);
+  //   }
+  // }
+  // return (
+  //   <><h1>Landing Page</h1><div>
+  //       <button type="submit" className="button-landpage" onClick={handleSignOut}> Log Out </button>
+  //     </div></>
 
-  );
+  // );
+
+  // return (
+  //   <>
+  //     <h1>Landing Page for Signed In users</h1>
+  //     <div>
+  //       <button
+  //         type="submit"
+  //         className="button-landpage"
+  //         // onClick={handleSignOut}
+  //       >
+  //         {" "}
+  //         Log Out{" "}
+  //       </button>
+  //     </div>
+  //   </>
+  // );
+   return (
+     <div>
+      <Dashboard/>
+
+     </div>
+   );
 };
 
 export default HomePage;
